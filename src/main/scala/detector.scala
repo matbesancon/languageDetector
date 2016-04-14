d5ae36a11e054e9441f5a1a5c471c43998a410f1
matbesancon <mathieu.besancon@gmail.com>
2016-04-14 23:59:38 +0200

    first working prototype

--- src/main/scala/detector.scala
+++ src/main/scala/detector.scala
@@ -4,15 +4,20 @@ import io.Source
 
 object Detector {
 
-  def main(args: Array[String]) = {
-    println("Compile?")
-  }
-
   // reading dictionnary
   def getDic = {
-    val readf = Source.fromFile("dict.txt").getLines.toList map {_.split(" ").toList}
+    val readf = Source.fromFile("src/main/dict.txt").getLines.toList map {_.split(" ").toList}
     val lang = readf.map(_.apply(0)).distinct
     lang.map((l:String)=>(l,readf filter{_(0)==l} map((lis)=>(lis(1)(0),lis(2).toDouble))toMap)).toMap
+  }
+  val dic = getDic
+  def main(args: Array[String]) = {
+    println("Starting program")
+    println("Possible languages")
+    println(dic.keys.mkString)
+    val text = io.StdIn.readLine
+    val l = find_language(text,dic)
+    println(l)
   }
 
   // methods building features
@@ -23,7 +28,7 @@ object Detector {
                                    text.mkString.length.toDouble)).toMap
   }
 
-  // finds the best fitting language from the frequency count and 
+  // finds the best fitting language from the frequency count and
   def find_language(text:String,countRef: Map[String, Map[Char, Double]]):
   Iterable[String] = {
     val c = countFreq(text.toList.map(_.toString))
