417db22a26765fa8a3e6cece79908ec757789d2d
matbesancon <mathieu.besancon@gmail.com>
2016-04-14 20:00:04 +0200

    refactor and comment key methods

--- src/main/scala/detector.scala
+++ src/main/scala/detector.scala
@@ -1,9 +1,18 @@
+import io.Source
+
 // TODO: way to store dictionnary in a text file?
 
 object Detector {
 
   def main(args: Array[String]) = {
     println("Compile?")
+  }
+
+  // reading dictionnary
+  def getDic = {
+    val readf = Source.fromFile("dict.txt").getLines.toList map {_.split(" ").toList}
+    val lang = readf.map(_.apply(0)).distinct
+    lang.map((l:String)=>(l,readf filter{_(0)==l} map((lis)=>(lis(1)(0),lis(2).toDouble))toMap)).toMap
   }
 
   // methods building features
@@ -13,12 +22,28 @@ object Detector {
     return count.map((c:Char)=>(c->text.mkString.count(_==c).toDouble/
                                    text.mkString.length.toDouble)).toMap
   }
-  // 2. letter transition frequency: Map (letter1,letter2)->frequency
-  def countTrans(text: List[String]):Map[(Char,Char),Double] = {
 
-  }
-  def find_language(text:String,countRef: Map[String, Map[Char, Double]]):Iterable[String] = {
+  // finds the best fitting language from the frequency count and 
+  def find_language(text:String,countRef: Map[String, Map[Char, Double]]):
+  Iterable[String] = {
     val c = countFreq(text.toList.map(_.toString))
-    return countRef.filter((count)=>compareFreq(count._2,c)==countRef.values.map(compareFreq(_,c)).min).keys
+    return countRef.filter((count)=>compareFreq(count._2,c)==
+      countRef.values.map(compareFreq(_,c)).min).keys
+  }
+
+  def compareFreq(countLang:Map[Char,Double],countText:Map[Char,Double]):
+  Double = {
+    return countLang.keys.map((c:Char)=>Math.abs(countLang.apply(c)-
+    countText.apply(c))).sum
   }
 }
+
+// 2. letter transition frequency: Map (letter1,letter2)->frequency
+// include letter transition from and to whitespace
+// def countTrans(text: List[String]):Map[(Char,Char),Double] = {
+//
+// }
+
+// object DicBuilder {
+//
+// }
