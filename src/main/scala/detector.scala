a7e8e0ebc87f86d145eafb06c65e2d405b13eae2
matbesancon <mathieu.besancon@gmail.com>
2016-04-16 15:25:04 +0200

    compute and print all languages

--- src/main/scala/detector.scala
+++ src/main/scala/detector.scala
@@ -6,9 +6,11 @@ object Detector {
 
   // reading dictionnary
   def getDic = {
-    val readf = Source.fromFile("src/main/dict.txt").getLines.toList map {_.split(" ").toList}
+    val readf = Source.fromFile("src/main/dict.txt").getLines.toList map {
+      _.split(" ").toList}
     val lang = readf.map(_.apply(0)).distinct
-    lang.map((l:String)=>(l,readf filter{_(0)==l} map((lis)=>(lis(1)(0),lis(2).toDouble))toMap)).toMap
+    lang.map((l:String)=>(l,readf filter{_(0)==l} map(
+      (lis)=>(lis(1)(0),lis(2).toDouble))toMap)).toMap
   }
 
   val dic = getDic
@@ -17,7 +19,12 @@ object Detector {
     println("Possible languages")
     dic.keys foreach println
     val text = io.StdIn.readLine
+    // computing note for each language
+    println("--- Languages scoring")
+    val notes = comp_language(text,dic)
+    notes.toSeq.sortWith(_._1 < _._1).foreach(tup=>println(""+tup._1+": "+tup._2))
     val l = find_language(text,dic)
+    println("----Decision----")
     print("Language: ")
     println(l)
     print("Score: ")
@@ -33,24 +40,25 @@ object Detector {
   }
 
   // Computes the score of each language for the text
-  // def comp_language(text:String,countRef: Map[String, Map[Char, Double]]):
-  // Iterable[String] = {
-  //   val c = countFreq(text.toList.map(_.toString))
-  //   return countRef map {tup => (tup,compareFreq(tup._2,c))}
-  // }
+  def comp_language(text:String,countRef: Map[String, Map[Char, Double]]):
+      Map[String,Double] = {
+    val cText = countFreq(text)
+    return countRef map {tup => (tup._1,compareFreq(tup._2,cText))}
+  }
 
   // finds the best fitting language from the frequency count and
   def find_language(text:String,countRef: Map[String, Map[Char, Double]]):
-  String = {
+    String = {
     val c = countFreq(text)
     return countRef.filter((count)=>compareFreq(count._2,c)==
       countRef.values.map(compareFreq(_,c)).min).keys.toList.apply(0)
   }
 
   def compareFreq(countLang:Map[Char,Double],countText:Map[Char,Double]):
-  Double = {
-    return countLang.keys.map((c:Char)=>Math.abs(countLang.apply(c)-
-    countText.apply(c))).sum
+    Double = {
+    val mapfreq = countLang.keys.map((c:Char)=>Math.abs(countLang.apply(c)-
+    countText.apply(c)))
+    return mapfreq.sum/mapfreq.size
   }
 
 }
