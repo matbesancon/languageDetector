8ac3602b5d21ac4074bac30fa46e848a41414be0
matbesancon <mathieu.besancon@gmail.com>
2016-04-18 19:27:17 +0200

    function refactoring, type acceptance

--- src/main/scala/detector.scala
+++ src/main/scala/detector.scala
@@ -1,7 +1,7 @@
 import io.Source
 import language.postfixOps
 
-// TODO: fix main to adapt to new function definitions
+// TODO: fix main to adapt to new function definition
 
 object Detector {
 
@@ -21,25 +21,27 @@ object Detector {
     dic.keys foreach println
     val text = io.StdIn.readLine
     // computing note for each language
-    println("--- Languages scoring: L1 ---")
-    val notes1 = comp_language(text,dic,compareL1)
-    notes1.toSeq.sortWith(_._1 < _._1).foreach(tup=>println(""+tup._1+": "+tup._2))
-    val l1 = find_language(text,dic,compareL1)
-    println("----Decision----")
-    print("Language: ")
-    println(l1)
-    print("Score: ")
-    println(compareL1(dic.apply(l1),countFreq(text)))
-
-    println("--- Languages scoring: L2 ---")
-    val notes2 = comp_language(text,dic,compareL2)
-    notes2.toSeq.sortWith(_._1 < _._1).foreach(tup=>println(""+tup._1+": "+tup._2))
-    val l2 = find_language(text,dic,compareL2)
-    println("----Decision----")
-    print("Language: ")
-    println(l2)
-    print("Score: ")
-    println(compareL2(dic.apply(l2),countFreq(text)))
+    // println("--- Languages scoring: L1 ---")
+    // val notes1 = comp_language(text,dic,compareL1)
+    // notes1.toSeq.sortWith(_._1 < _._1).foreach(tup=>println(
+    //   ""+tup._1+": "+tup._2))
+    // val l1 = find_language(text,dic,compareL1)
+    // println("----Decision----")
+    // print("Language: ")
+    // println(l1)
+    // print("Score: ")
+    // println(compareL1(dic.apply(l1),countFreq(text)))
+    //
+    // println("--- Languages scoring: L2 ---")
+    // val notes2 = comp_language(text,dic,compareL2)
+    // notes2.toSeq.sortWith(_._1 < _._1).foreach(tup=>println(
+    //   ""+tup._1+": "+tup._2))
+    // val l2 = find_language(text,dic,compareL2)
+    // println("----Decision----")
+    // print("Language: ")
+    // println(l2)
+    // print("Score: ")
+    // println(compareL2(dic.apply(l2),countFreq(text)))
 
   }
 
@@ -54,7 +56,8 @@ object Detector {
   def countTrans(text: String):Map[(Char,Char),Double] = {
     val letters = "abcdefghijklmnopqrtstuvwxyz".toList
     val transitions = letters map {letter => letters map {(letter,_)}} flatten
-    val textTrans = Range(0,text.length-1) map {ind:Int => (text.apply(ind),text.apply(ind+1))}
+    val textTrans = Range(0,text.length-1) map {ind:Int => (text.apply(ind),
+      text.apply(ind+1))}
     return transitions.map((t:(Char,Char))=>(t->textTrans.count(_==t).toDouble/
                                    textTrans.length.toDouble)).toMap
   }
@@ -80,7 +83,7 @@ object Detector {
   // finds the best fitting language from the score of each one
   def find_language(scores: Map[String,Double]):
     String = {
-    return scores.filter(_==scores.values.min).apply(0)
+    return scores.filter(_==scores.values.min).keys.head
   }
 
   def compareL1(countLang:Map[Equals,Double],countText:Map[Equals,Double]):
